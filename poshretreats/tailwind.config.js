3ebf8857e9c4bef1769b39cd48ea41f363154ad8
unrealtobi <tobiwunmie@gmail.com>
2024-11-23 11:54:59 +0100

    nice

--- poshretreats/tailwind.config.js
+++ poshretreats/tailwind.config.js
@@ -1,38 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
         smoothOpening: {
-          '0%': {
-            transform: 'scaleX(0)',
-            opacity: '0',
+          "0%": {
+            transform: "scaleX(0)",
+            opacity: "0",
           },
-          '100%': {
-            transform: 'scaleX(1)',
-            opacity: '1',
+          "100%": {
+            transform: "scaleX(1)",
+            opacity: "1",
           },
         },
       },
       animation: {
-        smoothOpening: 'smoothOpening 1s ease-in-out forwards',
+        smoothOpening: "smoothOpening 1s ease-in-out forwards",
+        scroll: "scroll 20s linear infinite",
+        "scroll-mobile": "scroll 30s linear infinite",
       },
       colors: {
         customGreen: "#0B4525",
         customDarkGreen: "#08311a",
         customPink: "#F7A49D",
         customWhite: "#FFFFFF",
-        customBg: "#FAFFFC"
+        customBg: "#FAFFFC",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
         raleway: ["Raleway", "sans-serif"],
       },
+      fontSize: {
+        "10xl": "140px", // Explicitly define the class
+      },
       screens: {
-        sm: { min: '768px', max: '1024px' },
-        tablet: { min: '768px', max: '1024px' },
-        md: '1025px' 
+        sm: { min: "768px", max: "1024px" },
+        tablet: { min: "768px", max: "1024px" },
+        md: { min: "1025px", max: "1440px" },
       },
     },
   },

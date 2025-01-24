1aa18aeffdc1d28a7c840f2a03d2257c29a2d502
unrealtobi <tobiwunmie@gmail.com>
2025-01-24 15:22:32 +0100

    de

--- poshretreats/blog-next/next.config.mjs
+++ poshretreats/blog-next/next.config.mjs
@@ -1,14 +1,14 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+import path from 'path';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src'); // Set up '@' alias to point to 'src'
+    config.resolve.alias['@'] = path.resolve('./src'); // Set up '@' alias to point to 'src'
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
